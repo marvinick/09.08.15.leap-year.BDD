873c58d6efa85ead3c3f09dca99c4d48d7a9af71
micah courey & marvin nikijuluw <student@epicodus.com>
2015-09-08 11:13:32 -0700

    fix .not bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0)) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0 ))  {
     return true;
   } else {
     return false;
@@ -14,7 +14,9 @@ $(document).ready(function() {
     $(".year").text(year);
     if (!result) {
       $(".not").text("not");
-    }
+  } else {
+      $(".not").text("not").remove();
+  }
 
     $("#result").show();
     event.preventDefault();
